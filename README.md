e7cf8c43efc78a00499717939a1a1eeefec89c2b
mattrichor <matt.a.geyer@gmail.com>
2022-07-29 09:46:07 -0400

    updated README

--- README.md
+++ README.md
@@ -1,54 +1,60 @@
-
-
-
 # The-Living-Room
+
 A localized social app for families.
+
 ## Date: 7/23/2022
+
 ### Author: Matthew Geyer
 
 ### [Instagram](https://www.instagram.com/mattrichor/) | [LinkedIn](https://www.linkedin.com/in/matthew-geyer-174644170/) | [GitHub](https://github.com/mattrichor)
 
-***
+---
 
-### ***Description***
-A localized social app for families to share things most important to them: pictures, recipes, memories, & plans. Will have a family tree lineage space to see pictures of ancestors, & be easy to navigate & simple with the intent of it being used regularly by older folk who didn't grow up with technology. Stretch goals include having a calendar for family events, and the ability to make a new account. 
-***
+### **_Description_**
 
-### ***Technologies Used***
-* MERN Stack
-    * Mongoose
-    * Express
-    * React JS
-    * Node JS
+A localized social app for families to share things most important to them: pictures, recipes, memories, & plans. Will have a family tree lineage space to see pictures of ancestors, & be easy to navigate & simple with the intent of it being used regularly by older folk who didn't grow up with technology. Stretch goals include having a calendar for family events, and the ability to make a new account.
 
-***
-### ***Getting Started***
+---
+
+### **_Technologies Used_**
+
+- MERN Stack
+  - Mongoose
+  - Express
+  - React JS
+  - Node JS
+
+---
+
+### **_Getting Started_**
 
 ##### Hiroku Deployment
 
-***
+---
 
 ##### Wireframe
+
 link here: (https://app.diagrams.net/#G1vQgPdJIvNU_WUQHBcMJXw6_7z_3P6RHo)
 
-***
-### ***Screenshots***
+---
 
-***
-### ***Task Lists***
+### **_Screenshots_**
+
+---
+
+### **_Task Lists_**
+
 ## Trello Board
+
 link here: ()
-- [ ] Backend with Multiple Profiles for Family,
-    - [ ] Contents include: Birthday(populates on calendar), picture, brief info, place to share fun anecdotes / memories
-- [ ] Social space for posts (living room)
-- [ ] Family Tree UI Used to Navigate Lineage
-- [ ] Calendar for Event Planning with RSVP button
 
-***
+- [x] Backend with Multiple Profiles for Family,
+  - [x] Contents include: Birthday(populates on calendar), picture, brief info, place to share fun anecdotes / memories
+- [x] Social space for posts (living room)
+- [x] Family Tree UI Used to Navigate Lineage
 
-### ***Credits***
+---
+
+### **_Credits_**
 
 - ME (this is my megalomaniac arc)
-
-
-
--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -1,26 +1,17 @@
-import React from "react"
-import { useState, useEffect, createRef} from "react"
-import axios from "axios"
-import PostCard from "./PostCard"
-
-
+import React from 'react'
+import { useState, useEffect, createRef } from 'react'
+import axios from 'axios'
+import PostCard from './PostCard'
 
 const BASE_URL = 'http://localhost:3001/api'
 
-
-
-
-
 const Post = () => {
-
   const [posts, setPosts] = useState([])
   const [posted, togglePosted] = useState(false)
-  
+  const [fam, setFam] = useState([])
+
   const [updatedTitle, setUpdatedTitle] = useState('')
   const [updatedDesc, setUpdatedDesc] = useState('')
-
-  // const [newTitle, setNewTitle] = useState('')
-  // const [newDescription, setNewDescription] = useState('')
 
   useEffect(() => {
     const getPosts = async () => {
@@ -31,34 +22,43 @@ const Post = () => {
     getPosts()
   }, [posted === true])
 
-
+  useEffect(() => {
+    const getFam = async () => {
+      const res = await axios.get(`${BASE_URL}/family`)
+      console.log(res)
+      setFam(res.data)
+      console.log(fam)
+    }
+    getFam()
+  }, [posted === true])
 
   const deletePost = async (_id) => {
-    let res = await axios.delete(`${BASE_URL}/delete-post/${_id}`)
-    .then((res) => {
-      console.log(res.status)
-      togglePosted(true)
-    })
+    let res = await axios
+      .delete(`${BASE_URL}/delete-post/${_id}`)
+      .then((res) => {
+        console.log(res.status)
+        togglePosted(true)
+      })
   }
 
-    return (
-      <div className="post-card">
-        {posts.map((res) => (
-          <PostCard 
-            key={res._id}
-            img={res.image}
-            title={res.title}
-            author={res.author}
-            time={res.time}
-            description={res.description}
-            _id={res._id}
-            likes={res.likes}
-            deletePost={() => deletePost(res._id)}
-            // updatePost={() => updatePost(res._id)}
-            togglePosted={() => togglePosted()}
-            ></PostCard>
-         ))}
-      </div>
-    )
-  }
-  export default Post
+  return (
+    <div className="post-card">
+      {posts.map((res) => (
+        <PostCard
+          key={res._id}
+          img={res.image}
+          title={res.title}
+          author={res.author}
+          time={res.time}
+          description={res.description}
+          _id={res._id}
+          likes={res.likes}
+          deletePost={() => deletePost(res._id)}
+          // updatePost={() => updatePost(res._id)}
+          togglePosted={() => togglePosted()}
+        ></PostCard>
+      ))}
+    </div>
+  )
+}
+export default Post
