e73f8074a83ce0ccddca989e5efec87f1adb2255
mattrichor <matt.a.geyer@gmail.com>
2022-09-20 14:01:05 -0400

    more cleaning up

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-import { StrictMode } from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom'
 
-ReactDOM.render(
-  <StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </StrictMode>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <Router>
+    <App />
+  </Router>
 )
--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import Axios from 'axios'
 
-export const BASE_URL = 'http://localhost:3001/api'
+// export const BASE_URL = 'http://localhost:3001/api'
 
-// export const BASE_URL = 'https://the-living-room-server.herokuapp.com/'
+export const BASE_URL = 'https://the-living-room-server.herokuapp.com/api'
 
 const Client = Axios.create({ baseURL: BASE_URL })
 
